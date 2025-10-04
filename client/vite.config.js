48709de651c39951ef9bbf3cce99a1adf9bb074f
addygit123 <minecraftaddy123gmail.com>
2025-10-04 13:53:07 +0530

    added the trip and reset features

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+  proxy: {
+    '/api': {
+      target: 'http://localhost:5001', // Your Express server port
+      changeOrigin: true,
+      secure: false,
+    },
+  },
+},
  }
 )
